c869275deeb41725caff1addbcee3daa074c59db
1oneisall <42429929+1oneisall@users.noreply.github.com>
2021-02-24 16:11:46 +0800

    Update bgtk.js

--- bgtk.js
+++ bgtk.js
@@ -1,4 +1,8 @@
-console.log("test");
+//console.log("test");
+var body = response.body;
+let obj = JSON.parse(body);
+obj.data.is_super_vip = 1;
+$done({body:JSON.stringify(obj)});
 //var ResponseHeaders;
 //RequestHeaders = $response.headers;
 //console.log(RequestHeaders);
