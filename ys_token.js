df7952bdc637e254dcf9a4a1375c08864fe664aa
1oneisall <42429929+1oneisall@users.noreply.github.com>
2021-09-27 15:38:45 +0800

    Update ys_token.js

--- ys_token.js
+++ ys_token.js
@@ -2,6 +2,7 @@ function Get_Ys_Token()
 {
     const Headers = $request.headers;
     const Token = Headers["token"];
+    console.log("Token");
     $persistentStore.write(Token, "Ys_Token");
     console.log("写入Ys Token成功:${Token}");
     $notification.post("写入Ys Token成功");
